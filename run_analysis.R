459776e869d2e95cf635270e0febcc499c04f472
Andrew Burnett <andrew@nospam.forme>
2014-07-09 22:03:36 +0100

    Add extract cols

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,11 @@
 ## nothing here yet
 
 # X - Download and extract dataset.
-# 1 - Merges the training and the test sets to create one data set.
-# 2 - Extracts only the measurements on the mean and standard deviation 
+# X - Merges the training and the test sets to create one data set.
+# X - Extracts only the measurements on the mean and standard deviation 
 #     for each measurement. 
-# 3 - Uses descriptive activity names to name the activities in the data set
-# 4 - Appropriately labels the data set with descriptive variable names. 
+# X - Uses descriptive activity names to name the activities in the data set
+# X - Appropriately labels the data set with descriptive variable names. 
 # 5 - Creates a second, independent tidy data set with the average of each 
 #     variable for each activity and each subject. 
 
@@ -25,8 +25,42 @@ getData <- function () {
   unzip(temp, exdir=dir)
 }
 
+# TODO: possible problem with activity matching up,also maybe subject?
 mergeData <- function() {
-  features = read.table("data/UCI HAR Dataset/features.txt")
+  features = read.table("data/UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
+  labels = read.table("data/UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE)
+  
   test_data = read.table("data/UCI HAR Dataset/test/X_test.txt")
   test_labels = read.table("data/UCI HAR Dataset/test/y_test.txt")
+  test_subjects = read.table("data/UCI HAR Dataset/test/subject_test.txt")
+  
+  test_activities = merge(test_labels, labels, by.x="V1", by.y="V1", sort=FALSE)
+  
+  test_data$subject <- test_subjects[[1]]
+  test_data$activity <- test_activities[[2]]
+  
+  train_data = read.table("data/UCI HAR Dataset/train/X_train.txt")
+  train_labels = read.table("data/UCI HAR Dataset/train/y_train.txt")
+  train_subjects = read.table("data/UCI HAR Dataset/train/subject_train.txt")
+  
+  train_activities = merge(train_labels, labels, by.x="V1", by.y="V1", sort=FALSE)
+  
+  train_data$subject <- train_subjects[[1]]
+  train_data$activity <- train_activities[[2]]
+  
+  mdata <- rbind(train_data, test_data)
+  names(mdata) <- c(features[[2]], "subject", "activity")
+  mdata
+}
+
+extractData <- function(data) {
+  msCols = grep("(mean|std)\\(\\)", names(data), value=TRUE, ignore.case=TRUE)
+  xdata = data[c(msCols, "subject", "activity")]
+  xnames = names(xdata)
+  xnames = tolower(xnames)
+  xnames = sub("\\(\\)", "", xnames)
+  xnames = sub("-(\\w+)-([XYZ])$", "_\\2-axis_\\1", xnames, ignore.case=TRUE)
+  xnames = sub("-([^a])", "_\\1", xnames)
+  names(xdata) <- xnames
+  xdata
 }
