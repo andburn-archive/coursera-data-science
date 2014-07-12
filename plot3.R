82a2856160c06907600b965311bf13777008f1ad
Andrew Burnett <andrew@nospam.forme>
2014-07-12 20:26:06 +0100

    Add plot 3

---
+++ plot3.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(reshape2)
+
+bl <- subset(NEI, fips == "24510")
+
+typeyr <- tapply(bl$Emissions, list(bl$year, bl$type), sum)
+
+types <- data.frame(year=row.names(typeyr), 
+                  typeyr, 
+                  row.names=c(1:4), 
+                  stringsAsFactors=F)
+
+mt <- melt(types, id.vars="year")
+mt$year <- as.integer(mt$year)
+
+gp <- ggplot(mt, aes(x=year, y=value, fill=variable)) 
+gp <- gp + geom_line() + geom_point(size=4, shape=21)
+gp <- gp + ggtitle("Total yearly PM2.5 emissions in Baltimore, MD by source")
+gp <- gp + xlab("Years") + ylab("PM2.5 Emissions (tons)") + labs(fill="Sources")
+print(gp)
